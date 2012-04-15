cda1622c7f31a4b4377677926f2283b4b8c92358
Ron Parker <rdparker@gmail.com>
2012-04-15 13:14:22 -0500

    Remove legacy items from original sample app

--- picture-viewer.lisp
+++ picture-viewer.lisp
@@ -4,25 +4,9 @@
 
 (enable-rdnzl-syntax)
 
-(import-types "System.Windows.Forms"
-	      "Application" "Form" "Button" "ListBox"
-	      "MessageBox" "MessageBoxButtons" "DialogResult")
-
-(import-type "System.EventHandler" (load-assembly "mscorlib"))
+(import-types "System.Windows.Forms" "Application")
 
 (use-namespace "System.Windows.Forms")
-(use-namespace "System")
-
-(let ((listbox1 (new "ListBox")))
-  (defun button1-click (sender e)
-    (declare (ignore sender e))
-    "Handle the Click event for button1"
-    (let ((count 1))
-      (loop while [Equals [MessageBox.Show "Exit application?" ""
-					   [$MessageBoxButtons.YesNo]]
-			  [$DialogResult.No]]
-	 do [Add [%Items listbox1] count]
-	   (incf count)))))
 
 (defun picture-viewer ()
   "Run the picture-viewer application"
